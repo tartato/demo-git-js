8559f666b3e421d02b69b4fa4642e37b45b3910d
Andres H <emianf@gmail.com>
2025-05-14 09:03:13 -0500

    update scriptjs

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("Hola desde JavaScript esto es una demo");
+function agregarElemento() {
+  const ul = document.getElementById("lista");
+  const li = document.createElement("li");
+  li.textContent = "Ítem nuevo";
+  ul.appendChild(li);
+}
+
+function limpiarLista() {
+  const ul = document.getElementById("lista");
+  ul.innerHTML = "";
+}
